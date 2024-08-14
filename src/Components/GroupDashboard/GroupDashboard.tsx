9c2b18e8362364e8fa58c63bb665434ff079566b
stevennguyen003 <96093087+stevennguyen003@users.noreply.github.com>
2024-08-14 18:02:48 -0400

    clean

--- src/Components/GroupDashboard/GroupDashboard.tsx
+++ src/Components/GroupDashboard/GroupDashboard.tsx
@@ -1,11 +1,7 @@
-import SettingsButton from "../SettingsButton/SettingsButton";
-import QuizHeader from "../QuizHeader/QuizHeader";
-import QuizBox from "../QuizBox/QuizBox";
 import Leaderboard from "../Leaderboard/Leaderboard";
 import UserActivity from "../UserActivity/UserActivity";
 import QuestionFlashcard from "../QuestionFlashcard/QuestionFlashcard";
 import * as groupClient from "../../Clients/groupClient";
-import * as noteClient from "../../Clients/noteClient"
 import "./index.css";
 
 interface GroupDashboardProps {
@@ -27,26 +23,3 @@ function GroupDashboard({ group, onUpdateGroup }: GroupDashboardProps) {
     );
 }
 export default GroupDashboard;
-
-
-
-// <>
-//     <div className="groupHeader">
-//         <div className="groupTitle">
-//             {group?.name}
-//         </div>
-//         <SettingsButton />
-//     </div>
-//     <hr />
-//     <div className="leaderInfo">
-//         <div className="bar" id="bar2">2nd</div>
-//         <div className="bar" id="bar1">1st</div>
-//         <div className="bar" id="bar3">3rd</div>
-//     </div>
-//     <div className="quizInfo">
-//         <QuizHeader />
-//     </div>
-//     <div className="quizMenu">
-//         <QuizBox />
-//     </div>
-// </>
